08f28742ca89e081abbe437db7bc639ac4f8fbd9
muchpretty99 <muchpretty99@ibizsoftware.net>
2023-07-26 16:51:58 +0900

    hooks 적용

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,56 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Square } from "./Square";
 import "./Board.css";
 
-export class Board extends React.Component {
-  /**
-   * constructor(생성자)를 사용하면 인스턴스화된 객체에서 다른 메서드를 호출하기
-   * 전에 수행해야 하는 사용자 지정 초기화를 제공할 수 있습니다.
-   */
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
-  handleClick(i) {
-    /**
-     * slice() 메서드는 어떤 배열의 begin부터 end까지에 대한
-     * shallow copy를 새로운 배열 객체로 반환합니다.
-     */
-    const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
-  }
-  renderSqaure(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-  render() {
-    const status = "Next player: X";
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSqaure(0)}
-          {this.renderSqaure(1)}
-          {this.renderSqaure(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSqaure(3)}
-          {this.renderSqaure(4)}
-          {this.renderSqaure(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSqaure(6)}
-          {this.renderSqaure(7)}
-          {this.renderSqaure(8)}
-        </div>
+const Board = () => {
+  const status = "Next player: X";
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = "X";
+    setSquares(newSquares);
+  };
+
+  const renderSqaure = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSqaure(0)}
+        {renderSqaure(1)}
+        {renderSqaure(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSqaure(3)}
+        {renderSqaure(4)}
+        {renderSqaure(5)}
+      </div>
+      <div className="board-row">
+        {renderSqaure(6)}
+        {renderSqaure(7)}
+        {renderSqaure(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
